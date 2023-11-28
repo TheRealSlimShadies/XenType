d1a31415d581e83c185d0e388920d2156b5514b0
RationallyPi <anwesh.atreya@gmail.com>
2023-11-28 13:56:29 +0545

    Added most of the functionalities to the buttons

--- xentype/mainwindow.h
+++ xentype/mainwindow.h
@@ -2,13 +2,15 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include<QTextEdit>
 #include<QFile>
 #include<QFileDialog>
 #include<QTextStream>
 #include<QMessageBox>
-
-
+#include<QColor>
+#include<QColorDialog>
+#include<QTextCursor>
+#include<QTextCharFormat>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -35,6 +37,30 @@ private slots:
     void on_actionUndo_triggered();
 
     void on_actionRedo_triggered();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_13_clicked();
+
+    void on_pushButton_14_clicked();
 
 private:
     Ui::MainWindow *ui;
